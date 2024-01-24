48596c823ffa84a4a5489d90cf44656879afa2b8
agiratech-harini <harini.r@agiratech.com>
2024-01-24 18:48:37 +0530

    checking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'first-component', component: FirstComponentComponent},
+{path: 'second-component', component: SecondComponentComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
