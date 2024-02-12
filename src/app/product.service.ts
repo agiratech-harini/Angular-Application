14b6d8acaa320981cfb5bf05d5db36100ef27409
agiratech-harini <harini.r@agiratech.com>
2024-02-12 18:39:37 +0530

    service added for ptoduct component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,4 +10,23 @@ export class ProductService {
   getProducts() {
     return this.http.get('https://dummyjson.com/products');
   }
+  addProducts(product: any) {
+    console.log("in service");
+    
+    return this.http.post('https://dummyjson.com/products/add', product);
+  }
+  updateProducts(id: any ,product:any){
+    console.log("updated");
+    return this.http.put(`https://dummyjson.com/products/${id}`,product);
+    
+  }
+  patchProduct(product:any){
+    console.log("patched");
+    return this.http.patch('https://dummyjson.com/products/1',product);
+  }
+  deleteProduct(id:any){
+    console.log("deleted");
+    return this.http.delete(`https://dummyjson.com/products/${id}`);
+    
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
 import { map } from 'rxjs';
-type mobile={
-  image:string,
-  brand:string,
-  price:number,
-  rating:number,
-  stock:number
+type mobile = {
+  image: string,
+  brand: string,
+  price: number,
+  rating: number,
+  stock: number
 }
 @Component({
   selector: 'app-product',
@@ -14,38 +14,96 @@ type mobile={
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  mobiles!:Array<any>;
+  mobiles!: Array<any>;
 
-  constructor(private productService : ProductService){}
+  constructor(private productService: ProductService) { }
   ngOnInit(): void {
-    this.showProductService();
+    this.showProduct();
+    this.addProduct();
+    this.updateProduct();
+    this.patchProduct();
+    this.deleteProduct();
   }
 
-  showProductService() {
+  showProduct() {
     this.productService.getProducts()
-    .pipe(
-      map((response: any) => {  
-        console.log("Before map: ", response);  
+      .pipe(
+        map((response: any) => {
+          console.log("Before map: ", response);
 
-        return response.products
-      }),
-      map((response: any) => {
-        console.log("Before map: ", response);  
-        return response.map((item: any) => {
-          return {
-            image: item.images[0],
-            brand: item.brand,
-            price: item.price,
-            rating:item.rating,
-            stock:item.stock
-          }
+          return response.products
+        }),
+        map((response: any) => {
+          console.log("Before map: ", response);
+          return response.map((item: any) => {
+            return {
+              image: item.images[0],
+              brand: item.brand,
+              price: item.price,
+              rating: item.rating,
+              stock: item.stock
+            }
+          })
         })
-      })
-    )
+      )
       .subscribe((resp: any) => {
         console.log("After map: ", resp);
-        this.mobiles = resp.splice(0,10);
-      console.log(this.mobiles);
+        this.mobiles = resp.splice(0, 10);
+        console.log(this.mobiles);
       });
   }
+  addProduct() {
+    console.log("comes to add");
+    
+    let addedProduct = {
+      brand:"Impression of Acqua Di Gio",
+      image:"https://cdn.dummyjson.com/product-images/11/1.jpg",
+      price:13,
+      rating:4.26,
+      stock:44
+    }
+    this.productService.addProducts(addedProduct).subscribe({
+      next: value => console.log('Success: ' + value),
+      error: err => console.error('Error: ' + err),
+    })
+  }
+  updateProduct() {
+    console.log("update");
+    
+    let updatedProduct = {
+      brand:"Impression of Acqua Di Gio",
+      image:"https://cdn.dummyjson.com/product-images/11/1.jpg",
+      price:13,
+      rating:4.26,
+      stock:44
+    }
+    let id = 1;
+    this.productService.updateProducts(id, updatedProduct).subscribe((response: any) => {
+      console.log("Response: ", response);
+      
+      
+    })
+  }
+  patchProduct(){
+  let patchedProduct = {
+    brand:"Impression of Acqua Di Gio",
+    image:"https://cdn.dummyjson.com/product-images/11/1.jpg",
+    price:13,
+    rating:4.26,
+    stock:44
+  }
+  this.productService.patchProduct(patchedProduct).subscribe((response:any)=>{
+   console.log("Response:",response);
+   
+  })
 }
+deleteProduct(){
+  let id=1;
+  this.productService.deleteProduct(id).subscribe((response:any)=>{
+    console.log("Response:",response);
+    
+  })
+
+  }
+}
+
